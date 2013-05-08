f57efe465e676c81d40b9ef27ab3a67fc0fb919e
priya-garg <priya.garg35@yahoo.com>
2013-05-08 11:00:31 -0700

    commmitttttt here

--- StackTest.php
+++ StackTest.php
@@ -2,14 +2,14 @@
 class StackTest extends PHPUnit_Framework_TestCase
 {
     public function testPushAndPop()
-    {  //test stack
+    {  // stack test here
         $stack = array();
         $this->assertEquals(0, count($stack));
  
         array_push($stack, 'foo');
         $this->assertEquals('foo', $stack[count($stack)-1]);
         $this->assertEquals(1, count($stack));
- 
+        //stack pop
         $this->assertEquals('foo', array_pop($stack));
         $this->assertEquals(0, count($stack));
     }
